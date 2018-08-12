2d4e19cc779d61e45e7dcfe525de29e9a4863d66
caty <catalina.acuna.g@gmail.com>
2018-08-12 15:00:24 -0300

    mantenedores de tipo_contacto listos

--- modules/tipo_contacto/editar.php
+++ modules/tipo_contacto/editar.php
@@ -1,19 +1,19 @@
 <?php
-	$id_comuna = isset($_GET['id'])? $_GET['id'] : NULL;
+	$id_cargo = isset($_GET['id'])? $_GET['id'] : NULL;
 	// verificar existencia de registro
 	$registro = [];
-	if(!empty($id_comuna)) {
-		$query = pg_query("SELECT nombre_comuna FROM comuna WHERE id_comuna=$id_comuna AND eliminado = false");
+	if(!empty($id_cargo)) {
+		$query = pg_query("SELECT nombre_cargo FROM tipo_contacto WHERE id_cargo=$id_cargo AND eliminado = false");
 		if(pg_num_rows($query) == 0) {
 			set_alert('El registro que intenta editar no existe.', 'danger');
-			header("Location: " . mod_link('comuna'));
+			header("Location: " . mod_link('tipo_contacto'));
 			die();
 		} else {
 			$registro = pg_fetch_assoc($query, 0);
 		}
 	} else {
 		set_alert('Id registro faltante.', 'danger');
-		header("Location: " . mod_link('comuna'));
+		header("Location: " . mod_link('tipo_contacto'));
 		die();
 	}
 
@@ -21,26 +21,26 @@
 		extract($registro);
 	} else {
 		extract($_POST);	
-		$query = "UPDATE comuna SET nombre_comuna = '$nombre_comuna' WHERE id_comuna = $id_comuna";
+		$query = "UPDATE tipo_contacto SET nombre_cargo = '$nombre_cargo' WHERE id_cargo= $id_cargo";
 		$result = pg_query($query);
 		if($result) {
-			set_alert('comuna editada correctamente.', 'success');
-			header("Location: " . mod_link('comuna'));
+			set_alert('Cargo editado correctamente.', 'success');
+			header("Location: " . mod_link('tipo_contacto'));
 			die();
 		}
 	}
 
-	$titulo = 'Editar Comuna';
+	$titulo = 'Editar Cargo';
 	include 'includes/header.php';
 ?>
 
-<form method="post" action="<?php echo mod_link('comuna', 'editar', $id_comuna); ?>" class="needs-validation" novalidate>
+<form method="post" action="<?php echo mod_link('tipo_contacto', 'editar', $id_cargo); ?>" class="needs-validation" novalidate>
 	<div class="form-row">
 		<div class="col-md-6 mb-3">
-		  	<label for="nombre_comuna">Nombre</label>
-		  	<input type="text" name="nombre_comuna" class="form-control" id="nombre_comuna" placeholder="Nombre" value="<?php ival('nombre_comuna', $nombre_comuna); ?>" required>
+		  	<label for="nombre_cargo">Cargo</label>
+		  	<input type="text" name="nombre_cargo" class="form-control" id="nombre_cargo" placeholder="Cargo" value="<?php ival('nombre_cargo', $nombre_cargo); ?>" required>
 		  	<div class="invalid-feedback">
-				Ingrese Nombre
+				Ingrese Cargo
 			</div>
 		</div>
 	</div>  	
