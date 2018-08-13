aad4b369fba207adb132aabc28e85272b3e4d70f
Carlos Acuña Campos <caacuna@webmachine.cl>
2018-08-12 23:07:06 -0300

    Agrega actividades a mantención

--- modules/calderas/agregar_mantencion.php
+++ modules/calderas/agregar_mantencion.php
@@ -21,9 +21,19 @@
 		extract($_POST);
 		$id_usuario = usuario('id');
 		$query = "INSERT INTO mantenciones(id_caldera, fecha_mant, id_usuario, comentario)
-			VALUES	($id_caldera, '$fecha_mant', $id_usuario, '$comentario');";
+			VALUES	($id_caldera, '$fecha_mant', $id_usuario, '$comentario')
+			RETURNING id_mantencion;";
 		$result = pg_query($query);
 		if($result) {
+			$mantencion = pg_fetch_object($result);
+			$id_mantencion = $mantencion->id_mantencion;
+			// guardo actividades
+			foreach ($actividades as $actividad) {
+				$id_act = (int) $actividad;
+				$query = "INSERT INTO actividades_mantencion(id_act, id_mantencion)
+					VALUES	($id_act, $id_mantencion);";
+				pg_query($query);
+			}
 			set_alert('Mantención agregada correctamente.', 'success');
 			header("Location: " . mod_link('calderas', 'ver', $id_caldera));
 			die();
@@ -33,9 +43,9 @@
 	$titulo = "Agregar Mantención a Caldera $id_caldera";
 	include 'includes/header.php';
 
-	// tecnicos para select
-	$query = pg_query("SELECT *, CONCAT(nombres, ' ', apellidos) AS nombre FROM usuario WHERE id_perfil IN(1, 2) AND eliminado = false");
-	$tecnicos = pg_fetch_all($query);
+	// actividades
+	$query = pg_query("SELECT * FROM actividades WHERE eliminado = false");
+	$actividades = pg_fetch_all($query);
 ?>
 
 <form method="post" action="<?php echo mod_link('calderas', 'agregar_mantencion', $id_caldera); ?>" class="needs-validation" novalidate>
@@ -48,6 +58,22 @@
 			</div>
 		</div>
 	</div>
+	<h4>Actividades</h4>
+	<div class="form-row">
+		<div class="col-md-6 mb-3">
+			<?php foreach($actividades as $actividad): ?>
+			<div class="form-check">	
+				<input type="checkbox" name="actividades[]" class="form-check-input" id="check<?php echo $actividad['id_act']; ?>" value="<?php echo $actividad['id_act']; ?>" required>
+				<label for="check<?php echo $actividad['id_act']; ?>" class="form-check-label"><?php echo $actividad['nombre_act']; ?></label>
+				<?php if ($actividad === end($actividades)): ?>
+				<div class="invalid-feedback">
+					Seleccione al menos una actividad
+				</div>
+				<?php endif; ?>
+			</div>
+			<?php endforeach; ?>
+		</div>
+	</div>	
 	<div class="form-row">
 		<div class="col-md-6 mb-3">
 			<label for="comentario">Comentario</label>
@@ -56,5 +82,17 @@
 	</div>
 	<button class="btn btn-primary" type="submit">Guardar</button>
 </form>
+
+<?php ob_start(); ?>
+<script>
+$(document).ready(function() {
+	// valida al menos una actividad seleccionada
+	$('.form-check-input').on('click', function() {
+		n_checked = $('.form-check-input:checked').length;
+		$('.form-check-input').prop('required', n_checked < 1);
+	});	
+});	
+</script>
+<?php $scripts = ob_get_clean(); ?>
 
 <?php include 'includes/footer.php'; ?>
